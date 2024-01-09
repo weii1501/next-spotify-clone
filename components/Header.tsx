f7509511763db41b238e6e6b1940b30d2ab64a43
weii1501 <vodacluc15@gmail.com>
2024-01-09 23:11:53 +0700

    day2

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,9 @@ import { useRouter } from "next/navigation";
 import React from "react";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -32,7 +35,33 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             <RxCaretRight size={35} className="text-white" />
           </button>
         </div>
+        <div className="flex md:hidden gap-x-2 items-center">
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <HiHome className="text-black" size={20} />
+          </button>
+          <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+            <BiSearch className="text-black" size={20} />
+          </button>
+        </div>
+        <div className="flex justify-between items-center gap-x-4">
+          <>
+            <div>
+              <Button
+                onClick={() => {}}
+                className="bg-transparent text-neutral-300 font-medium"
+              >
+                Đăng kí
+              </Button>
+            </div>
+            <div>
+              <Button onClick={() => {}} className="bg-white px-6 py-2">
+                Đăng nhập
+              </Button>
+            </div>
+          </>
+        </div>
       </div>
+      {children}
     </div>
   );
 };
